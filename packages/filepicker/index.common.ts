b28466e259a36fd877187417adb99214edc6d80f
Angel <angel@angelengineering.com>
2023-03-02 16:26:48 +0700

    update media types and Readme

--- packages/filepicker/index.common.ts
+++ packages/filepicker/index.common.ts
@@ -1,8 +1,9 @@
 export enum MediaType {
+  NONE = 0x00,
   IMAGE = 0x01,
   AUDIO = 0x02,
   VIDEO = 0x04,
-  DOCUMENT = 0x08,
-  ARCHIVE = 0x10,
-  ALL = IMAGE | AUDIO | VIDEO | DOCUMENT | ARCHIVE,
+  ARCHIVE = 0x08,
+  DOCUMENT = 0x10,
+  ALL = IMAGE | AUDIO | VIDEO | ARCHIVE | DOCUMENT,
 }
--- packages/filepicker/index.d.ts
+++ packages/filepicker/index.d.ts
@@ -1,12 +1,13 @@
 import { File } from '@nativescript/core';
 
 export enum MediaType {
+  NONE = 0x00,
   IMAGE = 0x01,
   AUDIO = 0x02,
   VIDEO = 0x04,
-  DOCUMENT = 0x08,
-  ARCHIVE = 0x10,
-  ALL = IMAGE | AUDIO | VIDEO | DOCUMENT | ARCHIVE,
+  ARCHIVE = 0x08,
+  DOCUMENT = 0x10,
+  ALL = IMAGE | AUDIO | VIDEO | ARCHIVE | DOCUMENT,
 }
 export function showPicker(type: MediaType, multiple: boolean): Promise<File[]>;
 export function getFreeMBs(filepath: string): number;
