0ce6b244e22dec194f2d5fb5bae688565e5fa0d3
Angel <angel@angelengineering.com>
2024-04-17 18:58:14 -0400

    update to 1.2.0
    fix promise-based functions
    fix events and show demo usage
    fix iOS caching for url audio files
    add intellisense comments
    add util to get duration from an audio file

--- packages/audio-player/README.md
+++ packages/audio-player/README.md
@@ -9,6 +9,9 @@
 This plugin provides an audio player for Android and iOS that supports playback of both local files and remote URL audio files.  For  **Android**,  MediaPlayer will internally cache remote files after first prepare/play. For **iOS**, the plugin will download and cache remote files on first prepare/play. 
 
 ## Contents
+
+- [NativeScript Audio Player](#audio-player)
+  - [Contents](#contents)
   - [Installation](#installation)
   - [Usage](#usage)
   - [Audio Player options](#supported-audio-player-options)
@@ -33,9 +36,13 @@ ns plugin add @angelengineering/audio-player
 The best way to understand how to use the plugin is to study the demo app included in this repo. You can see how the plugin is used in a TypeScript application by looking at `apps/demo/src/plugin-demos/audio-player.ts`.
 
 
-1. Import the plugin.
+1. Import the plugin and create a player instance.
 ```javascript
 import { AudioPlayer, AudioPlayerOptions } from '@angelengineering/audio-player';
+this.player = new AudioPlayer();
+this.player.on(AudioPlayer.completeEvent, () => {
+        console.log('playback complete');
+    });
 ```
 
 2. Play an audio file.
@@ -86,19 +93,22 @@ export interface AudioPlayerOptions {
    * Gets or sets the audio file url.
    */
   audioFile: string;
+  
+  /**
+   * Get or sets the player to loop playback.
+   */
+  loop: boolean;
+
+  /**
+   * enable/disable audio playback mixing with other active audio playback sources. Defaults to false.
+   */
+  audioMixing?: boolean;
 
   /**
    * Gets or sets the callback when the currently playing audio file completes.
    * @returns {Object} An object containing the native values for the callback.
    */
   completeCallback?: Function;
-
-  /**
-   * Get or sets the player to loop playback.
-   */
-  loop: boolean;
-
-  audioMixing?: boolean;
 
   /**
    * Gets or sets the callback when an error occurs with the audio player.
@@ -121,13 +131,20 @@ export interface AudioPlayer {
   /**
    * native instance getters
    */
-  readonly ios?: any;
-  readonly android?: any;
+  readonly ios?: any; //AVAudioPlayer
+  readonly android?: any; //android.media.MediaPlayer
 
   /**
-   * Volume getter/setter
+   * Volume supports values ranging from 0.0 for silence to 1.0 for full volume
    */
   volume: any;
+
+  /**
+   * Duration getter in milliseconds
+   *    Returns 0 if there is no audio file loaded
+   *    Returns -1 if there is an issue getting duration (Android)
+   */
+  duration: number;
 
   /**
    * Prepare Audio player by preloading an audio file from file oath or URL
@@ -142,12 +159,17 @@ export interface AudioPlayer {
   play(): Promise<boolean>;
 
   /**
-   * Pauses playing audio file.
+   * Pauses the currently playing audio file.
    */
   pause(): Promise<boolean>;
 
   /**
-   * Seeks to specific time.
+   * Resume audio player playback.
+   */
+  resume(): void;
+
+  /**
+   * Seeks to specific time, in ms, for the currently prepared audio file.
    */
   seekTo(time: number): Promise<boolean>;
 
@@ -162,19 +184,49 @@ export interface AudioPlayer {
   isAudioPlaying(): boolean;
 
   /**
-   * Get the duration of the audio file playing.
+   * Get the duration of the audio file playing, in ms (Promise version of duration property getter)
    */
-  getAudioTrackDuration(): Promise<string>;
+  getAudioTrackDuration(): Promise<number>;
 
   /**
-   * current time
+   * Get the current time position of the audio file playing in ms
    */
   readonly currentTime: number;
+
+   /**
+   * Sets the player playback speed rate. On Android this only works on API 23+.
+   * @param speed [number] - The speed of the playback.
+   * speed should be a float from 0.0 - X.X, and is a scale factor
+   */
+  changePlayerSpeed(speed: number): void;
+
+  /**
+   * Begins playback at a certain time, relative to the start of the audio track.
+   * @param time [number] - The time to start playing the audio track at.
+   */
+  playAtTime(time: number);
+  
+  /**
+   * Events
+   */
+  public static seekEvent = 'seekEvent';
+  public static pausedEvent = 'pausedEvent';
+  public static startedEvent = 'startedEvent';
+  public static completeEvent = 'completeEvent';
+  public static errorEvent = 'errorEvent'; //will pass the error object
 }
 ```
 
-Tested and working on Android API 25-33.
-Tested and working on iOS 12.x-16.x. 
+## Helper Utils
+``` javascript
+  /*
+  * Utility to find the duration in milliseconds of the mp4 file at `mp4Path`
+  */
+  export function getDuration(mp4Path: string): number;
+```
+
+Tested and working on Android API 25-34.
+Tested and working on iOS 12.x-17.x. 
 
 ## Acknowledgements
 
--- packages/audio-player/index.ios.ts
+++ packages/audio-player/index.ios.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
 import { knownFolders, Observable, path as nsFilePath, Utils, File } from '@nativescript/core';
 import { IAudioPlayer, resolveAudioFilePath } from './common';
 import { AudioPlayerOptions } from './options';
@@ -5,12 +6,12 @@ import { AudioPlayerOptions } from './options';
 declare const AVAudioPlayer;
 
 @NativeClass()
-class TNSPlayerDelegate extends NSObject implements AVAudioPlayerDelegate {
+class NSPlayerDelegate extends NSObject implements AVAudioPlayerDelegate {
   static ObjCProtocols = [AVAudioPlayerDelegate];
   private _owner: WeakRef<AudioPlayer>;
 
   static initWithOwner(owner: AudioPlayer) {
-    const delegate = <TNSPlayerDelegate>TNSPlayerDelegate.new();
+    const delegate = <NSPlayerDelegate>NSPlayerDelegate.new();
     delegate._owner = new WeakRef(owner);
     return delegate;
   }
@@ -18,25 +19,32 @@ class TNSPlayerDelegate extends NSObject implements AVAudioPlayerDelegate {
   audioPlayerDidFinishPlayingSuccessfully(player?: any, flag?: boolean) {
     const owner = this._owner.get();
     if (owner) {
+      owner._sendEvent(AudioPlayer.completeEvent);
       if (flag && owner.completeCallback) {
         owner.completeCallback({ player, flag });
+        if (owner._resolve) owner._resolve(true);
       } else if (!flag && owner.errorCallback) {
         owner.errorCallback({ player, flag });
+        if (owner._reject) owner._reject(flag);
       }
+      owner._reject = owner._resolve = null;
     }
   }
 
   audioPlayerDecodeErrorDidOccurError(player: any, error: NSError) {
     const owner = this._owner.get();
     if (owner) {
+      owner._sendEvent(AudioPlayer.errorEvent, error);
       if (owner.errorCallback) {
         owner.errorCallback({ player, error });
       }
+      if (owner._reject) owner._reject(error);
+      owner._reject = owner._resolve = null;
     }
   }
 }
 
-export { TNSPlayerDelegate };
+export { NSPlayerDelegate };
 
 export class AudioFocusManager extends Observable {}
 
@@ -48,18 +56,25 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
   private _player: AVAudioPlayer;
   private _task: NSURLSessionDataTask;
   private _data: NSData;
-  private delegate: TNSPlayerDelegate;
+  private delegate: NSPlayerDelegate;
   private _options: AudioPlayerOptions;
   private _readyToPlay = false;
+  _resolve = null;
+  _reject = null;
 
-  get ios(): any {
+  get ios(): AVAudioPlayer {
     return this._player;
   }
-
+  /**
+   * Returns volume with values ranging from 0.0 for silence to 1.0 for full volume.
+   */
   get volume(): number {
     return this._player ? this._player.volume : 0;
   }
 
+  /**
+   * Sets volume with values ranging from 0.0 for silence to 1.0 for full volume.
+   */
   set volume(value: number) {
     if (this._player && value >= 0) {
       this._player.volume = value;
@@ -70,44 +85,60 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
     return this._readyToPlay;
   }
 
+  /**
+  iOS returns the total duration, in seconds, of the player’s audio, which is converted to ms to match Android
+  * @property duration
+  */
   public get duration() {
     if (this._player) {
-      return this._player.duration;
+      return this._player.duration * 1000;
     } else {
       return 0;
     }
   }
 
+  /**
+   * iOS returns the current playback time, in seconds, within the audio timeline, which is converted to ms to match Android
+   * @property currentTime
+   */
   get currentTime(): number {
-    return this._player ? this._player.currentTime : 0;
+    return this._player ? this._player.currentTime * 1000 : 0;
   }
 
-  public setAudioFocusManager(manager: any) {
-    return true;
+  /**
+   * Android-only
+   */
+  public setAudioFocusManager(manager: any): void {
+    //not currently supported on iOS
+    return;
   }
 
+  /**
+   * This function will prepare the iOS audio player for playback using the specified AudioPlayerOptions.
+   * If the audio file name specified is a remote URL, it will attempt ot download the file and cache it future playback.
+   * @method prepareAudio
+   * @param options AudioPlayerOptions.
+   * @returns Promise that resolves as true once prepared successfully, or false if there was an error.
+   */
   public prepareAudio(options: AudioPlayerOptions): Promise<boolean> {
     return new Promise((resolve, reject) => {
       try {
         this._options = options;
         const audioPath = resolveAudioFilePath(options.audioFile);
-
-        if (Utils.isFileOrResourcePath(audioPath)) {
-          //if it's a local file, prepare should be almost instant
-          if (!File.exists(audioPath)) {
-            console.error("Audio file doesn't exist on device file system");
-            reject('File not found! ' + audioPath);
-            return;
-          }
+        const that = this;
+        /**
+         * Handler function once we have a file ready to load into the iOS audio player
+         */
+        const handleFileReady = function () {
           try {
             let fileName = Utils.isString(options.audioFile) ? options.audioFile.trim() : '';
             if (fileName.indexOf('~/') === 0) {
               fileName = nsFilePath.join(knownFolders.currentApp().path, fileName.replace('~/', ''));
             }
 
-            this.completeCallback = options.completeCallback;
-            this.errorCallback = options.errorCallback;
-            this.infoCallback = options.infoCallback;
+            that.completeCallback = options.completeCallback;
+            that.errorCallback = options.errorCallback;
+            that.infoCallback = options.infoCallback;
 
             const audioSession = AVAudioSession.sharedInstance();
             if (options.audioMixing) {
@@ -130,7 +161,6 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
             }
 
             const output = audioSession.currentRoute.outputs.lastObject.portType;
-
             if (output.match(/Receiver/)) {
               try {
                 audioSession.setCategoryError(AVAudioSessionCategoryPlayAndRecord);
@@ -142,47 +172,58 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
             }
 
             const errorRef = new interop.Reference();
-            this._player = AVAudioPlayer.alloc().initWithContentsOfURLError(NSURL.fileURLWithPath(fileName), errorRef);
+            that._player = AVAudioPlayer.alloc().initWithContentsOfURLError(NSURL.fileURLWithPath(fileName), errorRef);
             if (errorRef && errorRef.value) {
-              this._readyToPlay = false;
+              that._readyToPlay = false;
               reject(errorRef.value);
               return;
-            } else if (this._player) {
-              if (this.delegate === undefined) this.delegate = TNSPlayerDelegate.initWithOwner(this);
-              this._player.delegate = this.delegate;
-              // enableRate to change playback speed
-              this._player.enableRate = true;
+            } else if (that._player) {
+              if (that.delegate === undefined) that.delegate = NSPlayerDelegate.initWithOwner(that);
+              that._player.delegate = that.delegate;
+              // enableRate to allow change of playback speed
+              that._player.enableRate = true;
 
               if (options.loop) {
-                this._player.numberOfLoops = -1;
+                that._player.numberOfLoops = -1;
               }
 
-              this._readyToPlay = true;
+              that._readyToPlay = true; //audio player is ready to play the file
               resolve(true);
             } else {
               reject(false);
             }
           } catch (ex) {
-            if (this.errorCallback) {
-              this.errorCallback({ ex });
+            if (that.errorCallback) {
+              that.errorCallback({ ex });
             }
-            this._readyToPlay = false;
+            that._readyToPlay = false;
             reject(ex);
           }
+        };
+        if (Utils.isFileOrResourcePath(audioPath)) {
+          //if it's a local file, prepare should be almost instant
+          if (!File.exists(audioPath)) {
+            console.error("Audio file doesn't exist on device file system");
+            reject('File not found! ' + audioPath);
+            return;
+          }
+          handleFileReady();
         } else {
+          //check if we've already downloaded the file from this url and use that if so
           let cachefilename: string = audioPath.replaceAll('://', '_').replaceAll('.', '_').replaceAll('/', '-');
           cachefilename =
             knownFolders.temp().path + '/' + cachefilename.substring(0, cachefilename.lastIndexOf('_')) + '.' + cachefilename.substring(cachefilename.lastIndexOf('_') + 1, cachefilename.length);
+          console.log('remote url used for playback', audioPath);
+          console.log('downloading to local file if not cached already', cachefilename);
           const localFile = File.fromPath(cachefilename);
           if (File.exists(cachefilename) && localFile.size > 100) {
-            this._data = localFile.readSync();
-            resolve(true);
+            console.log('found a cached file, using that to prepare audio player');
+            this._options.audioFile = cachefilename;
+
+            handleFileReady();
           } else {
-            //for url, need to wait for urlsession to load and return
+            //otherwise for a url, need to wait for urlsession to load and return the file
             try {
-              this.completeCallback = options.completeCallback;
-              this.errorCallback = options.errorCallback;
-              this.infoCallback = options.infoCallback;
               this._task = NSURLSession.sharedSession.dataTaskWithURLCompletionHandler(NSURL.URLWithString(options.audioFile), (data, response, error) => {
                 if (error !== null) {
                   if (this.errorCallback) {
@@ -196,44 +237,18 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
                 const f = File.fromPath(cachefilename);
                 f.writeSync(NSData.dataWithData(data), (e: any) => {
                   console.error('Failed to write data: ' + e.toString());
-                  reject('Failed to write data: ' + e.toString());
+                  reject('Failed to write data during download: ' + e.toString());
                 });
-                let cacheFile = File.fromPath(cachefilename);
+                console.log('finished downloading file from url', f.path, f.size);
+                const cacheFile = File.fromPath(cachefilename);
                 if (cacheFile.size < 100) {
                   console.error('Downloaded file too small, failed?', cacheFile.size);
                   reject('Remote url file invalid! ' + audioPath);
                   return;
                 }
-                const audioSession = AVAudioSession.sharedInstance();
-                if (options.audioMixing) {
-                  audioSession.setCategoryWithOptionsError(AVAudioSessionCategoryAmbient, AVAudioSessionCategoryOptions.MixWithOthers);
-                } else {
-                  audioSession.setCategoryWithOptionsError(AVAudioSessionCategoryAmbient, AVAudioSessionCategoryOptions.DuckOthers);
-                }
-                const output = audioSession.currentRoute.outputs.lastObject.portType;
-                if (output.match(/Receiver/)) {
-                  try {
-                    audioSession.setCategoryError(AVAudioSessionCategoryPlayAndRecord);
-                    audioSession.overrideOutputAudioPortError(AVAudioSessionPortOverride.Speaker);
-                    audioSession.setActiveError(true);
-                  } catch (err) {
-                    console.error('Setting audioSession category failed.', err);
-                  }
-                }
-                const errorRef = new interop.Reference();
-                this._player = AVAudioPlayer.alloc().initWithDataError(data, errorRef);
-                if (errorRef && errorRef.value) {
-                  reject(errorRef.value);
-                  return;
-                } else if (this._player) {
-                  this._player.delegate = TNSPlayerDelegate.initWithOwner(this);
-                  // enableRate to change playback speed
-                  this._player.enableRate = true;
-                  this._player.numberOfLoops = options.loop ? -1 : 0;
-                  resolve(true);
-                } else {
-                  reject();
-                }
+
+                this._options.audioFile = cachefilename;
+                handleFileReady();
               });
 
               this._task.resume();
@@ -246,23 +261,28 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
             }
           }
         }
-
-        // resolve(true);
       } catch (ex) {
         reject(ex);
       }
     });
   }
 
-  public pause(): Promise<any> {
+  /**
+   * Pauses the currently playing audio file.
+   * @method pause
+   * @returns Promise that resolves as true once pause is complete, or false if there was an error during pause.
+   */
+  public pause(): Promise<boolean> {
     return new Promise((resolve, reject) => {
       try {
         if (this._player && this._player.playing) {
           this._player.pause();
+          this._sendEvent(AudioPlayer.pausedEvent);
         }
         resolve(true);
       } catch (ex) {
         if (this.errorCallback) {
+          this._sendEvent(AudioPlayer.errorEvent, ex);
           this.errorCallback({ ex });
         }
         reject(ex);
@@ -270,7 +290,12 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
     });
   }
 
-  public play(): Promise<any> {
+  /**
+   * Play current audio file that has been prepared by calling prepareAudio(options).
+   * @method play
+   * @returns Promise that resolves as true once playback is complete, or false if there was an error during playback.
+   */
+  public play(): Promise<boolean> {
     return new Promise((resolve, reject) => {
       try {
         if (!this.ready) {
@@ -279,9 +304,12 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
         }
         if (!this.isAudioPlaying()) {
           this._player.play();
+          this._sendEvent(AudioPlayer.startedEvent);
+          this._resolve = resolve;
+          this._reject = reject;
         }
-        resolve(true);
       } catch (ex) {
+        this._sendEvent(AudioPlayer.errorEvent, ex);
         if (this.errorCallback) {
           this.errorCallback({ ex });
         }
@@ -290,23 +318,42 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
     });
   }
 
+  /**
+   * Resume audio player playback.
+   * @method resume
+   */
   public resume(): void {
     if (this._player) {
       this._player.play();
+      this._sendEvent(AudioPlayer.startedEvent);
     }
   }
 
+  /**
+   * Plays audio asynchronously, starting at a specified point in the audio output device’s timeline.
+   * iOS expects this in seconds so we convert time param from ms first.
+   * @param time [number] - The time to start playing the audio track at, in milliseconds
+   */
   public playAtTime(time: number): void {
     if (this._player) {
-      this._player.playAtTime(time);
+      this._player.playAtTime(time / 1000);
+      this._sendEvent(AudioPlayer.startedEvent);
     }
   }
 
-  public seekTo(time: number): Promise<any> {
+  /**
+   * for iOS, currentTime either gets the current playback time, in seconds, within the audio timeline if playing,
+   *    or if not playing, sets playback at the specified time before next play() call
+   *  Convert from ms before sending to AVAudioPlayer
+   * * @method seekTo
+   * @returns Promise that resolves as true once seek is complete, or false if there was an error during seek.
+   */
+  public seekTo(time: number): Promise<boolean> {
     return new Promise((resolve, reject) => {
       try {
         if (this._player) {
-          this._player.currentTime = time;
+          this._player.currentTime = time / 1000;
+          this._sendEvent(AudioPlayer.seekEvent);
         }
         resolve(true);
       } catch (ex) {
@@ -315,7 +362,12 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
     });
   }
 
-  public dispose(): Promise<any> {
+  /**
+   * Releases resources from the audio player.
+   * @method dispose
+   * @returns Promise that resolves as true once disposal is complete, or false if there was an error during disposal.
+   */
+  public dispose(): Promise<boolean> {
     return new Promise((resolve, reject) => {
       try {
         if (this._player && this.isAudioPlaying()) {
@@ -324,7 +376,7 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
         const audioSession = AVAudioSession.sharedInstance();
         audioSession.setActiveError(false);
         this._reset();
-        resolve(null);
+        resolve(true);
       } catch (ex) {
         if (this.errorCallback) {
           this.errorCallback({ ex });
@@ -334,15 +386,25 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
     });
   }
 
+  /**
+   * Check if the audio is actively playing.
+   * @method isAudioPlaying
+   * @returns true if audio is playing, false if not.
+   */
   public isAudioPlaying(): boolean {
     return this._player ? this._player.playing : false;
   }
 
-  public getAudioTrackDuration(): Promise<string> {
+  /**
+   * Get the duration of the audio file prepared in player, in ms.
+   * @method getAudioTrackDuration
+   * @returns Promise that resolves the duration in ms, or 0 if there was an error when reading duration from native player.
+   */
+  public getAudioTrackDuration(): Promise<number> {
     return new Promise((resolve, reject) => {
       try {
-        const duration = this._player ? this._player.duration : 0;
-        resolve(duration.toString());
+        const duration = this._player ? this._player.duration * 1000 : 0;
+        resolve(duration);
       } catch (ex) {
         if (this.errorCallback) {
           this.errorCallback({ ex });
@@ -352,7 +414,12 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
     });
   }
 
-  public changePlayerSpeed(speed) {
+  /**
+   * Sets the player playback speed rate. On Android this works on API 23+.
+   * @param speed [number] - The speed of the playback.
+   * speed should be a float from 0.0 - X.X, and is a scale factor
+   */
+  public changePlayerSpeed(speed): void {
     if (this._player && speed) {
       // make sure speed is a number/float
       if (typeof speed === 'string') {
@@ -378,4 +445,36 @@ export class AudioPlayer extends Observable implements IAudioPlayer {
       this._task = undefined;
     }
   }
+
+  /**
+   * Events
+   */
+
+  /**
+   * Notify events by name and optionally pass data
+   */
+  public _sendEvent(eventName: string, data?: any) {
+    this.notify(<any>{
+      eventName,
+      object: this,
+      data: data,
+    });
+  }
+  public static seekEvent = 'seekEvent';
+  public static pausedEvent = 'pausedEvent';
+  public static startedEvent = 'startedEvent';
+  public static completeEvent = 'completeEvent';
+  public static errorEvent = 'errorEvent'; //will pass the error object
+}
+
+/**
+ * Utility to find the duration in milliseconds of the mp4 file at `mp4Path`
+ * @param mp4Path
+ */
+export function getDuration(mp4Path: string): number {
+  let totalTime = 0;
+  const filePath = NSURL.fileURLWithPath(mp4Path);
+  const avAsset = AVURLAsset.assetWithURL(filePath);
+  totalTime = CMTimeGetSeconds(avAsset.duration) * 1000;
+  return totalTime;
 }
