360682bb35f024beef7c73c35c1fcca1989c8904
Angel <angel@angelengineering.com>
2024-04-20 14:43:16 -0400

    Bump demo package versions. Update Android build settings and permissions.

--- apps/demo/src/plugin-demos/filepicker.ts
+++ apps/demo/src/plugin-demos/filepicker.ts
@@ -1,6 +1,8 @@
-import { EventData, Page, File, Frame, StackLayout, Color, Label, Image, alert, isAndroid, Device, knownFolders } from '@nativescript/core';
+/* eslint-disable @nrwl/nx/enforce-module-boundaries */
+import { EventData, Page, File, Frame, StackLayout, Color, Label, Image, alert, isAndroid, Device } from '@nativescript/core';
 import { DemoSharedFilepicker } from '@demo/shared';
 import { filePicker, galleryPicker, MediaType, getFreeMBs } from '@angelengineering/filepicker';
+import { TempFile } from '@angelengineering/filepicker/files';
 import { checkMultiple, check as checkPermission, request, request as requestPermission } from '@nativescript-community/perms';
 
 export function navigatingTo(args: EventData) {
@@ -8,98 +10,6 @@ export function navigatingTo(args: EventData) {
   page.bindingContext = new DemoModel();
 }
 export class DemoModel extends DemoSharedFilepicker {
-  async pickDoc() {
-    let pickedFiles: File[];
-    try {
-      pickedFiles = await filePicker(MediaType.DOCUMENT, false);
-    } catch (err) {
-      if (err) alert(err?.message);
-    } finally {
-      this.handleFiles(pickedFiles);
-    }
-  }
-
-  async pickImage() {
-    let pickedFiles: File[];
-    try {
-      pickedFiles = await filePicker(MediaType.IMAGE, false);
-    } catch (err) {
-      if (err) {
-        console.error(err);
-        alert(err?.message);
-      }
-    } finally {
-      this.handleFiles(pickedFiles);
-    }
-  }
-
-  async pickImages() {
-    let pickedFiles: File[];
-    try {
-      pickedFiles = await filePicker(MediaType.IMAGE, true);
-    } catch (err) {
-      if (err) {
-        console.error(err);
-        alert(err?.message);
-      }
-    } finally {
-      this.handleFiles(pickedFiles);
-    }
-  }
-
-  async pickVideo() {
-    let pickedFiles: File[];
-
-    try {
-      let tempPath = knownFolders.temp().path;
-      let freeSpace = getFreeMBs(tempPath);
-
-      console.log('free MBs on file picker temp directory', freeSpace);
-      console.log('temp directory path: ', tempPath);
-      if (freeSpace > 400) {
-        //check before allowing picker to create temp copy of selected files
-        pickedFiles = await filePicker(MediaType.VIDEO, false);
-      } else alert('Low free space on device, picking not allowed');
-    } catch (err) {
-      if (err) {
-        console.error(err);
-        alert(err?.message);
-      }
-    } finally {
-      this.handleFiles(pickedFiles);
-    }
-  }
-
-  async pickAudio() {
-    let pickedFiles: File[];
-
-    try {
-      pickedFiles = await filePicker(MediaType.AUDIO, false);
-    } catch (err) {
-      if (err) {
-        console.error(err);
-        alert(err?.message);
-      }
-    } finally {
-      this.handleFiles(pickedFiles);
-    }
-  }
-
-  async pickArchive() {
-    let pickedFiles: File[];
-
-    try {
-      pickedFiles = await filePicker(MediaType.ARCHIVE, false);
-    } catch (err) {
-      if (err) {
-        console.error(err);
-        alert(err?.message);
-      }
-    } finally {
-      this.handleFiles(pickedFiles);
-    }
-  }
-
   async pickAllOne() {
     let pickedFiles: File[];
 
@@ -130,8 +40,8 @@ export class DemoModel extends DemoSharedFilepicker {
           });
         }
         console.log('canPick?:', canPick);
-      } else if (isAndroid) {
-        //just request external_storage perms otherwise
+      } else if (isAndroid && +Device.sdkVersion < 26) {
+        //request external_storage perms for API <26 devices when targeting API34+
         const result = await checkPermission('storage');
         if (result['storage'] != 'authorized') console.log('No storage permission, requesting...');
         await request('storage').then(result => {
@@ -142,10 +52,7 @@ export class DemoModel extends DemoSharedFilepicker {
       if (canPick) pickedFiles = await filePicker(MediaType.ALL, false);
       else return alert('Need permissions before picking! Try again or update in app privacy settings first');
     } catch (err) {
-      if (err) {
-        console.error(err);
-        alert(err?.message);
-      }
+      if (err) alert(err?.message);
     } finally {
       this.handleFiles(pickedFiles);
     }
@@ -181,8 +88,8 @@ export class DemoModel extends DemoSharedFilepicker {
           });
         }
         console.log('canPick?:', canPick);
-      } else if (isAndroid) {
-        //just request external_storage perms otherwise
+      } else if (isAndroid && +Device.sdkVersion < 26) {
+        //request external_storage perms for API <26 devices when targeting API34+
         const result = await checkPermission('storage');
         if (result['storage'] != 'authorized') console.log('No storage permission, requesting...');
         await request('storage').then(result => {
@@ -193,10 +100,7 @@ export class DemoModel extends DemoSharedFilepicker {
       if (canPick) pickedFiles = await filePicker(MediaType.ALL, true);
       else return alert('Need permissions before picking! Try again or update in app privacy settings first');
     } catch (err) {
-      if (err) {
-        console.error(err);
-        alert(err?.message);
-      }
+      if (err) alert(err?.message);
     } finally {
       this.handleFiles(pickedFiles);
     }
@@ -213,10 +117,7 @@ export class DemoModel extends DemoSharedFilepicker {
             try {
               pickedFiles = await galleryPicker(MediaType.IMAGE + MediaType.VIDEO, true);
             } catch (err) {
-              if (err) {
-                console.error(err);
-                alert(err?.message);
-              }
+              if (err) alert(err?.message);
             } finally {
               this.handleFiles(pickedFiles);
             }
@@ -224,6 +125,85 @@ export class DemoModel extends DemoSharedFilepicker {
         });
       } else alert("No permission for files, can't open picker. Grant this permission in app settings first and then try again");
     });
+  }
+
+  //The following examples may require permissions depending on the platform, OS version and build target,
+  //   check the Readme for more information or look at the examples above.
+  async pickDoc() {
+    let pickedFiles: File[];
+    try {
+      pickedFiles = await filePicker(MediaType.DOCUMENT, false);
+    } catch (err) {
+      if (err) alert(err?.message);
+    } finally {
+      this.handleFiles(pickedFiles);
+    }
+  }
+
+  async pickImage() {
+    let pickedFiles: File[];
+    try {
+      pickedFiles = await filePicker(MediaType.IMAGE, false);
+    } catch (err) {
+      if (err) alert(err?.message);
+    } finally {
+      this.handleFiles(pickedFiles);
+    }
+  }
+
+  async pickImages() {
+    let pickedFiles: File[];
+    try {
+      pickedFiles = await filePicker(MediaType.IMAGE, true);
+    } catch (err) {
+      if (err) alert(err?.message);
+    } finally {
+      this.handleFiles(pickedFiles);
+    }
+  }
+
+  async pickVideo() {
+    let pickedFiles: File[];
+
+    try {
+      const tempPath = TempFile.getPath('tempfile', 'tmp');
+      const freeSpace = getFreeMBs(tempPath);
+
+      console.log('free MBs on file picker temp directory', freeSpace);
+      console.log('temp directory path: ', tempPath);
+      if (freeSpace > 400) {
+        //check before allowing picker to create temp copy of selected files
+        pickedFiles = await filePicker(MediaType.VIDEO, false);
+      } else alert('Low free space on device, picking not allowed');
+    } catch (err) {
+      if (err) alert(err?.message);
+    } finally {
+      this.handleFiles(pickedFiles);
+    }
+  }
+
+  async pickAudio() {
+    let pickedFiles: File[];
+
+    try {
+      pickedFiles = await filePicker(MediaType.AUDIO, false);
+    } catch (err) {
+      if (err) alert(err?.message);
+    } finally {
+      this.handleFiles(pickedFiles);
+    }
+  }
+
+  async pickArchive() {
+    let pickedFiles: File[];
+
+    try {
+      pickedFiles = await filePicker(MediaType.ARCHIVE, false);
+    } catch (err) {
+      if (err) alert(err?.message);
+    } finally {
+      this.handleFiles(pickedFiles);
+    }
   }
 
   handleFiles(results: File[]): void {
